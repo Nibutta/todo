8e8567ae4f11ec6f457300fd0b452d0b43cbe3e9
Dunice <dunice@Mac-mini-Dunice.local>
2017-03-21 16:59:11 +0300

    March 21 upd 3

--- scripts/example.js
+++ scripts/example.js
@@ -1,23 +1,23 @@
-/**
- * Created by Peter Dee on 21.03.17.
- */
-function addListItem() {
-    var content = $('#newItem').val();
-    $('#items_wrap').append('<div><div class="item_inner_wrap"><input type="checkbox" class="checkMark"/><span>'
-        + content + '</span></div><div class="removeItem"></div></div>');
-    $('#newItem').val('');
+function addListItem()
+{
+    var text = $('#newItem').val();
+    $("#items_wrap").append('<div><input type="checkbox" class="checkMark"/><span class="text">'
+        + text + ' </span><button class="removeItem"></button></div>');
+    $("#newItem").val('');
+}
+function clearCompleted()
+{
+    $("#items_wrap .checkMark:checked").parent().remove();
 }
 
-function removeCompleted() {
-    $('#items_wrap .checkMark:checked').parent().remove();
-}
-
-function deleteItem () {
+function deleteItem()
+{
     $(this).parent().remove();
 }
-
-function completedItems() {
-    if ($(this).parent().css('textDecoration') == 'line-through')
+function completed() {
+    if
+    (
+        $(this).parent().css('textDecoration') == 'line-through' )
     {
         $(this).parent().css('textDecoration', 'none');
         $(this).parent().css('opacity', '1');
@@ -28,19 +28,20 @@ function completedItems() {
         $(this).parent().css('opacity', '0.50');
     }
 }
-
-$(document).ready(function() {
+$(document).ready(function(){
     $('#newItem').keyup(function(e)
+    {
+        if (e.keyCode === 13)
+
         {
-            if (e.keyCode === 13)
-            {
-                addListItem();
-            }
-        });
+            addListItem();
+        }
+    });
     $(document).on('click', '.removeItem', deleteItem);
-    $('#markAll').click(function() {
+    $("#markAll").click(function ()
+    {
         $('input:checkbox').not(this).prop('checked', this.checked);
-        if ($('div').css('textDecoration') == 'line-through')
+        if ( $('div').css('textDecoration') == 'line-through' )
         {
             $('div').css('textDecoration', 'none');
             $('div').parent().css('opacity', '1');
@@ -51,22 +52,24 @@ $(document).ready(function() {
             $('div').parent().css('opacity', '0.5');
         }
     });
-    $(document).on('click', '.checkMark', completedItems);
-    $('#clearComp').click(removeCompleted);
+    $(document).on('click', '.checkMark', completed);
+    $("#clearButton").click(clearCompleted);
     $('#items_wrap').on('dblclick', 'span', function ()
         {
             var thisData = this.innerHTML,
                 $el = $('<input type="text" class="editItem"/>');
             $(this).replaceWith($el);
             $el.val(thisData).focus();
-            $(this).find(".content").hide();
+            $(this).find(".text").hide();
             $(this).find(".removeItem").hide();
-        });
+        }
+    );
     $('#items_wrap').on('keyup', '.editItem', (function(e)
     {
         if (e.keyCode === 13)
         {
-            $(this).replaceWith($('<span>' + $(this).val() + '</span>'));
+            $(this).replaceWith($('<span class="text">' + $(this).val() + '</span>'));
+
         }
         if (e.keyCode == 27) {
             $('.editItem').remove();
